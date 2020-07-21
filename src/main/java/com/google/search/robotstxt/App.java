75cb1a4ca2590441632419decf87407cc51ea3eb
Edu Pereda <edupereda@google.com>
2020-07-21 11:49:04 +0200

    Add maven configuration support for proto buffers.
    Also, document how to use it in the README.md file.

--- src/main/java/com/google/search/robotstxt/App.java
+++ src/main/java/com/google/search/robotstxt/App.java
@@ -1,8 +1,13 @@
 package com.google.search.robotstxt;
 
+import com.google.search.robotstxt.example.ExampleProtos.Example;
+
 /** Hello world! */
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    Example aProto = Example.newBuilder().setSomeField("Helo World!").addSomeRepeated(42).build();
+
+    System.out.println("My proto:\n" + aProto);
   }
 }
