f2132f4d7de7e270e20a71d200804920221d5152
andreeadutulescu <andreea99dutulescu@gmail.com>
2020-07-24 13:59:34 +0000

    Added TestsResult class that holds the results of the tests performed.

---
+++ src/main/java/com/google/search/robotstxt/spec/TestsResult.java
@@ -0,0 +1,80 @@
+package com.google.search.robotstxt.spec;
+
+import com.google.search.robotstxt.spec.testfile.TestFileProtos;
+import java.util.ArrayList;
+
+/**
+ * Holds the results of the tests performed
+ */
+public class TestsResult {
+  private int totalNumberComplianceTests;
+  private int totalNumberUserTests;
+  private int numberFailedComplianceTests;
+  private int numberFailedUserTests;
+  ArrayList<Pair<TestInfo, TestFileProtos.Outcome>> failedComplianceTests;
+  ArrayList<Pair<TestInfo, TestFileProtos.Outcome>> failedUserTests;
+
+  /**
+   * Default constructor
+   */
+  public TestsResult() {}
+
+  /**
+   * Reports the success of one Complience Test
+   */
+  public void reportSuccessComplianceTests() {
+    this.totalNumberComplianceTests++;
+  }
+
+  /**
+   * Reports the success of one user test
+   */
+  public void reportSuccessUserTests() {
+    this.totalNumberUserTests++;
+  }
+
+  /**
+   * Adds the failure of one Compliance Test
+   * @param failedTest The info about the failed test
+   * @param userOutcome The outcome of the parser
+   */
+  public void reportFailureComplianceTests(
+      TestInfo failedTest, TestFileProtos.Outcome userOutcome) {
+    this.totalNumberComplianceTests++;
+    this.numberFailedComplianceTests++;
+    this.failedComplianceTests.add(new Pair(failedTest, userOutcome));
+  }
+
+  /**
+   * Adds the failure of one user test
+   * @param failedTest The info about the failed test
+   * @param userOutcome The outcome of the parser
+   */
+  public void reportFailureUserTests(TestInfo failedTest, TestFileProtos.Outcome userOutcome) {
+    this.totalNumberUserTests++;
+    this.numberFailedUserTests++;
+    this.failedUserTests.add(new Pair(failedTest, userOutcome));
+  }
+
+  @Override
+  public String toString() {
+    String result =
+        "TEST RESULTS: \n\n"
+            + "Number of Compliance Tests: "
+            + this.totalNumberComplianceTests
+            + "\nNumber of Compliance Tests Failed: "
+            + this.failedComplianceTests
+            + "\nNumber of user's tests: "
+            + this.totalNumberUserTests
+            + "\nNumber of user's tests failed: "
+            + this.numberFailedUserTests;
+
+    if (this.numberFailedComplianceTests == 0) {
+      result = result + "\n\nThe parser FOLLOWS the standard";
+    } else {
+      result = result + "\n\nThe parser DOES NOT FOLLOW the standard";
+    }
+
+    return result;
+  }
+}
